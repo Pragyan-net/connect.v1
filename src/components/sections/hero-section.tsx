99ac982d9ae0c4cd9966be4d4758f21a80d2734e
Pragyan Dev <pragyanminal@gmail.com>
2026-01-05 11:14:18 +0000

    make the hero full sized  (_for element div_)

--- src/components/sections/hero-section.tsx
+++ src/components/sections/hero-section.tsx
@@ -1,6 +1,6 @@
 export function HeroSection() {
   return (
-    <section className="relative flex h-screen w-full items-center justify-center overflow-hidden bg-black">
+    <section className="relative flex h-dvh w-full items-center justify-center overflow-hidden bg-black">
       <div className="absolute inset-0 bg-grid-white/[0.05] [mask-image:linear-gradient(to_bottom,white_10%,transparent_90%)]"></div>
       <h1 className="font-headline text-5xl text-center md:text-8xl lg:text-9xl neon-text select-none">
         The Initiative.V1
